c493139a1f644a34a0f5baf173c31ddba7ad2be8
Alperen Mutlu <chetto_17@hotmail.com>
2021-06-09 23:14:49 +0300

    Güncellendi.

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Dropdown, Menu, Image } from "semantic-ui-react";
 
-export default function SignedIn() {
+export default function SignedIn({ signOut }) {
   return (
     <div>
       <Menu.Item>
@@ -10,10 +10,10 @@ export default function SignedIn() {
           spaced="right"
           src="https://yt3.ggpht.com/ytc/AAUvwngwJNZN3WObqCSP3MlLYGiR3cGFXbOkO3vWScSp2A=s900-c-k-c0x00ffffff-no-rj"
         />
-        <Dropdown pointing="top left" text="Alperen">
+        <Dropdown pointing="top left" text="Engin">
           <Dropdown.Menu>
             <Dropdown.Item text="Bilgilerim" icon="info" />
-            <Dropdown.Item text="Çıkış Yap" icon="sign-out" />
+            <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out" />
           </Dropdown.Menu>
         </Dropdown>
       </Menu.Item>
